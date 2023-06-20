fee373b0740639f5bcab7d9c338ca481428eb008
virghiu <virghiu@gmail.com>
2023-06-19 20:12:03 -0400

    feat: add and checking element argument

--- element.sh
+++ element.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+  read ELEMENT
+else
+  ELEMENT=$1
+fi
+
+echo $ELEMENT
