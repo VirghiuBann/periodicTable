c5b6f2d890ad2c2a8f76e2c44f72771d40b98f5f
virghiu <virghiu@gmail.com>
2023-06-19 20:38:33 -0400

    feat: add query to get the element

--- element.sh
+++ element.sh
@@ -2,12 +2,20 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+MAIN() {
+  # get result of the element
+  GET_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1 OR symbol='$1' OR name='$1'")
+
+  echo $GET_RESULT
+ }
+
 if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
   read ELEMENT
 else
-  ELEMENT=$1
+  ELEMENT=$1  
 fi
 
-echo $ELEMENT
+MAIN $ELEMENT
+
