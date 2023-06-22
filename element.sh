f76e21c159f3aa5d27a31bcbff6d736ef940ed11
virghiu <virghiu@gmail.com>
2023-06-22 16:17:39 -0400

    feat: add feature to show element detail

--- element.sh
+++ element.sh
@@ -10,7 +10,12 @@ MAIN() {
   then
     echo "I could not find that element in the database."
   else
-    echo $GET_ATOMIC_NUMBER
+    GET_ELEMENT=$($PSQL "SELECT  atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$GET_ATOMIC_NUMBER")
+
+    IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS TYPE <<< "$GET_ELEMENT"
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+
   fi
 
  }
