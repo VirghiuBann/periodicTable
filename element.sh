6d15293146c76007da341756ee640e6bcb57f345
virghiu <virghiu@gmail.com>
2023-06-22 16:44:24 -0400

    refactor: add query functions
    refactor: add get_atomic_number function
    
    refactor: add get_element function
    
    add print_element_details function

--- element.sh
+++ element.sh
@@ -2,22 +2,35 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-MAIN() {
-  # get result of the element
-  GET_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE (symbol='$1' OR name='$1' OR atomic_number::text= '$1')")
+GET_ATOMIC_NUMBER() {
+  local query="SELECT atomic_number FROM elements WHERE (symbol='$1' OR name='$1' OR atomic_number::text= '$1')"
+  $PSQL "$query"
+}
 
-  if [[ -z $GET_ATOMIC_NUMBER ]]
+GET_ELEMENT() {
+  local query="SELECT  atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1"
+  $PSQL "$query"
+}
+
+PRINT_ELEMENT_DETAILS() {
+    IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS TYPE <<< "$1"
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+}
+
+MAIN() {
+  local ELEMENT=$1
+  
+  local GET_ATOMIC_NUMBER_RESULT=$(GET_ATOMIC_NUMBER "$ELEMENT")
+ 
+  if [[ -z $GET_ATOMIC_NUMBER_RESULT ]]
   then
     echo "I could not find that element in the database."
   else
-    GET_ELEMENT=$($PSQL "SELECT  atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$GET_ATOMIC_NUMBER")
+    local GET_ELEMENT_RESULT=$(GET_ELEMENT "$GET_ATOMIC_NUMBER_RESULT")
 
-    IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS TYPE <<< "$GET_ELEMENT"
-
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-
+    PRINT_ELEMENT_DETAILS "$GET_ELEMENT_RESULT"
   fi
-
  }
 
 if [[ -z $1 ]]
