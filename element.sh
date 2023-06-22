a14987108980919c598d7e316dc4537a95baf895
virghiu <virghiu@gmail.com>
2023-06-22 15:33:27 -0400

    feat: get atomic number

--- element.sh
+++ element.sh
@@ -4,13 +4,13 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 MAIN() {
   # get result of the element
-  GET_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1 OR symbol='$1' OR name='$1'")
+  GET_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE (symbol='$1' OR name='$1' OR atomic_number::text= '$1')")
 
-  if [[ -z $GET_RESULT ]]
+  if [[ -z $GET_ATOMIC_NUMBER ]]
   then
     echo "I could not find that element in the database."
   else
-    echo $GET_RESULT
+    echo $GET_ATOMIC_NUMBER
   fi
 
  }
